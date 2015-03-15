d238ebf09e4d405edf976196c322a6e6ebec4144
christoffer-Van <christoffer.jjohansen@gmail.com>
2015-03-14 19:36:48 -0700

    intro page svg anim working

--- css/js/main.js
+++ css/js/main.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
   
   $('body').menuItems({});
 
-  new Vivus('test', {type: 'async', duration: 100});
+  $("#svg_real_logo").hide();
+
+  new Vivus('svg_logo', {type: 'oneByOne', duration: 160});
+  setTimeout( function(){
+    $("#svg_logo").fadeOut(500);
+    $("#svg_real_logo").fadeIn(500);
+  }, 3000);
+
 
 });
 
